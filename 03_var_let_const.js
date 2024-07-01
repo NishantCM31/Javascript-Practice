4dc77ef9dccdbfc787bcfbeb82b407d83af964ee
Nishant <nishantcm31@gmail.com>
2024-07-01 22:48:59 +0530

    3 and

---
+++ 03_var_let_const.js
@@ -0,0 +1,14 @@
+// var a = 45;
+// var a = "p"
+let b = "Nishant";
+const author = "Nishant"
+//author = 5 // Throws an error because constant cannot be changed
+b = 4
+const Nishant = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
---
+++ 04_primitives_objects.js
@@ -0,0 +1,19 @@
+// nn bb ss u - Primitives in Js
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567") + BigInt("3")
+let e = "Nishant"
+let f = Symbol("I am a nice symbol")
+let g
+console.log(a, b, c , d, e, f, g)
+console.log(typeof c)
+
+// Non Primitive Data Type - Objects in Js
+const item = {
+  "Nishant": true,
+  "Adarsh": false,
+  "Seshwar": 67,
+  "Suryatej": undefined
+}
+console.log(item["Adarsh"])
